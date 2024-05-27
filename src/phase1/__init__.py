e787b9cf514fb3273d73b9f404068d4e1d5cf029
purplelemons-dev <ironbreaker2005@gmail.com>
2024-05-26 20:37:53 -0500

    proof of concept works

--- src/phase1/__init__.py
+++ src/phase1/__init__.py
@@ -1,5 +1,6 @@
 
 import discord
+import json
 
 client = discord.Client()
 
@@ -11,10 +12,33 @@ async def on_ready():
 
     _channel = client.get_channel(client._channel_id)
 
+    data = []
+    counter = 0
+
+    async for message in _channel.history(limit=10**4):
+        if message.content:
+            message_data = {
+                "author": message.author.name,
+                "content": message.content,
+                "timestamp": message.created_at.timestamp()
+            }
+            data.append(message_data)
+        counter += 1
+        if counter % 100 == 0:
+            print(f"Processed {counter} messages")
+        
+        print("done!")
+
+    with open("phase1/data.json", "w") as f:
+        json.dump(data, f)
 
 
 def main():
-    bot_token = input("Enter the bot token: ")
-    client._channel_id = int(input("Enter the channel ID: "))
+    #bot_token = input("Enter the bot token: ")
+    #client._channel_id = int(input("Enter the channel ID: "))
+    with open("phase1/secrets.json") as f:
+        secrets = json.load(f)
+        bot_token = secrets["token"]
+        client._channel_id = int(secrets["channel"])
 
     client.run(bot_token)
